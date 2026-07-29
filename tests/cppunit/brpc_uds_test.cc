0dad062109d4647166b0beeea59557da92f823cb
shixianjie <shixianjie1@huawei.com>
2026-07-29 10:17:28 +0800

    test: cover brpc UDS socket cleanup

---
+++ tests/cppunit/brpc_uds_test.cc
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <brpc/server.h>
+#include <gtest/gtest.h>
+
+#include <unistd.h>
+
+#include <string>
+
+TEST(BrpcUds, RemovesSocketPathOnStop) {
+  const std::string socket_path = "/tmp/xbase-brpc-uds-test-" + std::to_string(getpid()) + ".sock";
+  const std::string endpoint = "unix:" + socket_path;
+  unlink(socket_path.c_str());
+
+  brpc::Server server;
+  ASSERT_EQ(server.Start(endpoint.c_str(), nullptr), 0);
+  ASSERT_EQ(access(socket_path.c_str(), F_OK), 0);
+  ASSERT_EQ(server.Stop(0), 0);
+  ASSERT_EQ(server.Join(), 0);
+  EXPECT_NE(access(socket_path.c_str(), F_OK), 0);
+
+  unlink(socket_path.c_str());
+}
